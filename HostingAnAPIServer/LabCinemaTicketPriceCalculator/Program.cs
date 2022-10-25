04f9dfc4ed3c6b613dd8cc96f7ee9f85fe18bfcd
QACS-TL <114167055+QACS-TL@users.noreply.github.com>
2022-10-25 11:06:58 +0100

    Add files via upload

---
+++ HostingAnAPIServer/LabCinemaTicketPriceCalculator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using LabCinemaTicketPriceCalculator;
+using Newtonsoft.Json;
+
+namespace LabCinemaTicketPriceCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple listener
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Prefixes.Add("http://localhost:8000/");
+                listener.Start();
+                while (true)
+                {
+                    HttpListenerContext context = listener.GetContext(); // Wait for request
+
+                    // Extracting information from the request URL
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    if (context == null) { 
+                        Console.WriteLine("Context is null");
+                        return;
+                    }
+                    sb.AppendLine($"Host: {context.Request.Url.Host}");
+                    sb.AppendLine($"Document: {context.Request.Url.AbsolutePath}");
+
+
+
+                    CustomerTicket ticket = new CustomerTicket();
+                    ticket.AdultCount = 0;
+                    ticket.ChildCount = 0;
+                    if (context.Request is not null)
+                    {
+                        Console.WriteLine("Context.Request is null");
+                        return;
+                    }
+                    ticket.AdultCount = int.Parse(context.Request.QueryString["AdultCount"]);
+                    ticket.ChildCount = int.Parse(context.Request.QueryString["ChildCount"]);
+
+                    string responseText = sb.ToString();
+
+                    // Extracting the payload body
+                    string bodyText = null;
+                    if (context.Request.HasEntityBody)
+                    {
+                        using (StreamReader reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+                        {
+                            bodyText = reader.ReadToEnd();
+                        }
+                    }
+
+                    dynamic filmDetails = JsonConvert.DeserializeObject(bodyText);
+                    byte[] responseBytes = null;
+                    context.Response.StatusCode = 200;
+                    if (filmDetails != null && filmDetails.film_id != null && filmDetails.date != null & filmDetails.time != null)
+                    {
+                        //TODO: Code that looks up file title would go here
+                        if (filmDetails.film_id < 500)
+                            ticket.Title = "It's a Wonderful Life";
+                        else
+                            ticket.Title = "Carry on carrying on";
+
+                        ticket.Date = filmDetails.date;
+                        ticket.Time = filmDetails.time;
+                        responseText += $"Booking is for \"{ticket.Title}\" on {filmDetails.date} at {filmDetails.time}"
+                                        + $" and will cost {ticket.TotalPrice:C}";
+                        responseBytes = System.Text.Encoding.UTF8.GetBytes(responseText);
+                        context.Response.StatusCode = 200;
+                        context.Response.StatusDescription = "OK";
+                    }
+                    else
+                    {
+                        responseText += "There were NO film details passed in a JSON format within the request body"
+                                     + "\n Format should be {\"film_id\":\"nnnn\", \"date\":\"HH/MM/YYYY\", \"time\":\"nn:nn\"}";
+                        responseBytes = System.Text.Encoding.UTF8.GetBytes(responseText);
+                        context.Response.StatusCode = 400;
+                        context.Response.StatusDescription = "Badness";
+                    }
+                    context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+                    context.Response.OutputStream.Close();
+                }
+                listener.Stop();
+            }
+        }
+    }
+}
---
+++ UsingHttpClientLab/HttpClientCommentsForPosts/HttpClientCommentsForPosts/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace HttpClientCommentsForPosts
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Outdoor cinema weather check
+            // Create an HTTP client (should only have one of these for whole application)
+            var http = new HttpClient();
+
+            //// Send a GET request and process the response as JSON, using C# dynamic objects
+            string city = "Leeds";
+            string url = $"https://weather-api.qaalabs.com/api/weather/{city}";
+            string json = http.GetStringAsync(url).Result;
+            dynamic obj = JsonConvert.DeserializeObject(json);
+            string temp = obj["TemperatureInCelsius"] >= 15 ? "warm" : "cold";
+            Console.WriteLine($"The weather for the outdoor cinema event " +
+                $"in {obj["City"]} is {obj["WeatherDescription"]} and it will be {temp}.");
+
+
+            // Sending headers with request, and extracting headers from response
+            var blog = new
+            {
+                title = "My movie blog post - Up",
+                body = "Up is an uplifting film",
+                userId = 101
+            };
+            string dataJson = JsonConvert.SerializeObject(blog);
+            url = "https://jsonplaceholder.typicode.com/posts";
+            HttpContent content = new StringContent(dataJson, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = http.PostAsync(url, content).Result;
+            string responseJson = response.Content.ReadAsStringAsync().Result;
+            dynamic responseData = JsonConvert.DeserializeObject(responseJson);
+            Console.WriteLine($"");
+            Console.WriteLine($"New blog post has ID: {responseData["id"]}");
+            Console.WriteLine($"New blog post has title text of: {responseData["title"]}");
+            Console.WriteLine($"New blog post has body text of: {responseData["body"]}");
+            Console.WriteLine($"New blog post has userID of: {responseData["userId"]}");
+
+            //******************************************************************************
+
+            int postId = 5;
+            url = $@"https://jsonplaceholder.typicode.com/posts/{postId}";
+            content = new StringContent(dataJson, Encoding.UTF8, "application/json");
+            response = http.GetAsync(url).Result;
+            responseJson = response.Content.ReadAsStringAsync().Result;
+            dynamic post = JsonConvert.DeserializeObject(responseJson, typeof(Post));
+            Console.WriteLine($"");
+            Console.WriteLine($"New blog post has an ID of {post.Id}");
+            Console.WriteLine($"New blog post has userID of {post.UserId}");
+            Console.WriteLine($"New blog post has title text of {post.Title}");
+            Console.WriteLine($"New blog post has body text of {post.Body}");
+
+            url = $@"https://jsonplaceholder.typicode.com/posts/{postId}/comments";
+            content = new StringContent(dataJson, Encoding.UTF8, "application/json");
+            response = http.GetAsync(url).Result;
+            responseJson = response.Content.ReadAsStringAsync().Result;
+            dynamic comments = JsonConvert.DeserializeObject(responseJson, typeof(List<Comment>));
+
+            Console.WriteLine($"\nPost {postId} has the following comments:");
+            foreach (Comment comment in comments)
+            {
+                Console.WriteLine($"From: {comment.Name}, Comment: {comment.Body}");
+            }
+        }
+    }
+}
